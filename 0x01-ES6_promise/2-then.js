ce5f6c11eaf691475eeba8f904aee745a8ea86ef
AlvyneZ <alvynew@gmail.com>
2023-11-30 01:41:58 +0300

    Handlers returning values

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,8 +1,7 @@
 export default function handleResponseFromAPI(promise) {
+  promise.then(() => ({ status: 200, body: 'Success' }));
+  promise.catch(() => Error());
   promise.finally(() => {
     console.log('Got a response from the API');
-  });
-  promise.catch(() => {
-    throw Error();
   });
 }
