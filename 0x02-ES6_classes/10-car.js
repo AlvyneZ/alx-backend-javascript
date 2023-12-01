42f499180868c6caf7efa76ad11128caea78affb
AlvyneZ <alvynew@gmail.com>
2023-12-01 22:01:07 +0300

    Creating a clone of a class

---
+++ 0x02-ES6_classes/10-car.js
@@ -0,0 +1,47 @@
+export default class Car {
+  constructor(brand, motor, color) {
+    this._brand = brand;
+    this._motor = motor;
+    this._color = color;
+  }
+
+  get brand() {
+    return this._brand;
+  }
+
+  set brand(newBrand) {
+    if (typeof (newBrand) !== 'string') {
+      throw new TypeError('Brand must be a string');
+    } else {
+      this._brand = newBrand;
+    }
+  }
+
+  get motor() {
+    return this._motor;
+  }
+
+  set motor(newMotor) {
+    if (typeof (newMotor) !== 'string') {
+      throw new TypeError('Motor must be a string');
+    } else {
+      this._motor = newMotor;
+    }
+  }
+
+  get color() {
+    return this._color;
+  }
+
+  set color(newColor) {
+    if (typeof (newColor) !== 'string') {
+      throw new TypeError('color must be a string');
+    } else {
+      this._color = newColor;
+    }
+  }
+
+  cloneCar() {
+    return new this.constructor();
+  }
+}
