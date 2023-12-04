07adeca8cb2c5ebba7a93570b237e12c5f0074e8
AlvyneZ <alvynew@gmail.com>
2023-12-04 22:04:15 +0300

    Array from Set and joining

---
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,16 @@
+export default function cleanSet(aSet, startString) {
+  if (startString && typeof startString === 'string') {
+    return Array.from(aSet)
+      .map(
+        (value) => {
+          if (value.startsWith(startString)) {
+            return value.slice(startString.length);
+          }
+          return null;
+        },
+      )
+      .filter((value) => value)
+      .join('-');
+  }
+  return '';
+}
