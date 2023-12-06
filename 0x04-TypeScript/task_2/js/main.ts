5c0431874111fd2084579a16dd2870d0fd61cecd
AlvyneZ <alvynew@gmail.com>
2023-12-06 22:48:21 +0300

    Extending an instance

---
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -0,0 +1,21 @@
+interface Teacher {
+  readonly firstName: string;
+  readonly lastName: string;
+  fullTimeEmployee: boolean;
+  yearsOfExperience?: number;
+  location: string;
+  [x: string]: unknown;
+}
+
+interface Directors extends Teacher {
+  numberOfReports: number;
+}
+
+const director1: Directors = {
+  firstName: 'John',
+  lastName: 'Doe',
+  location: 'London',
+  fullTimeEmployee: true,
+  numberOfReports: 17,
+};
+console.log(director1);
