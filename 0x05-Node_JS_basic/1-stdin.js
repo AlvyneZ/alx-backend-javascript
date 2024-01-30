bafabd4694911b811783d2f4393143feb7e42eff
AlvyneZ <alvynew@gmail.com>
2024-01-30 22:40:11 +0300

    stdin.on() eventhandlers

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,14 @@
+function main() {
+  console.log('Welcome to Holberton School, what is your name?');
+  process.stdin.on('readable', () => {
+    const name = process.stdin.read();
+    if (name) {
+      process.stdout.write(`Your name is: ${name}`);
+    }
+  });
+  process.stdin.on('end', () => {
+    console.log('This important software is now closing');
+  });
+}
+
+main();
