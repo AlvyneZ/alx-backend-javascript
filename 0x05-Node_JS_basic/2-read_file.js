27c070940d9189aad92ed2d2cb0df8615ce3cf36
AlvyneZ <alvynew@gmail.com>
2024-01-30 23:12:34 +0300

    Using fs module (synchronous)

---
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -0,0 +1,31 @@
+const fs = require('fs');
+
+function countStudents(path) {
+  if ((!fs.existsSync(path)) || (!fs.statSync(path).isFile())) {
+    throw new Error('Cannot load the database');
+  }
+  const fileContent = fs.readFileSync(
+    path, 'utf-8',
+  ).toString().trim().split('\n');
+
+  console.log(`Number of students: ${fileContent.length}`);
+  const studentFields = {};
+  const fileColumns = fileContent[0].split(',');
+  const firstNameColumn = fileColumns.indexOf('firstname');
+  const fieldColumn = fileColumns.indexOf('field');
+
+  for (const studentRecord of fileContent.slice(1)) {
+    const student = studentRecord.split(',');
+    if (!(student[fieldColumn] in studentFields)) {
+      studentFields[student[fieldColumn]] = [student[firstNameColumn]];
+    } else {
+      studentFields[student[fieldColumn]].push(student[firstNameColumn]);
+    }
+  }
+
+  for (const [field, group] of Object.entries(studentFields)) {
+    console.log(`Number of students in ${field}: ${group.length}. List: ${group.join(', ')}`);
+  }
+}
+
+module.exports = countStudents;
